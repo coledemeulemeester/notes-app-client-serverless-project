94dd11fc49350b007b9490e2f0728f7ac65936b8
coledemeulemeester <cmd328@cornell.edu>
2021-06-09 16:18:15 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-cole-tutorial",
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "uf2sfet95i",
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_f6KKi2sfV",
+      APP_CLIENT_ID: "10l6qceu8fmomnjfqg7tb5um68",
+      IDENTITY_POOL_ID: "us-east-1:309ac535-9bfc-45c9-a68f-8b14557c3a1c",
+    },
+  };
+  
+  export default config;
